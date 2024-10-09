949b462561f59e65fff9b607fd82bf0f4f5bacf2
PeyHunter <Nando.goat@gmail.com>
2024-10-09 10:12:21 +0200

    Testing GitHub

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,5 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String semikolon = ";";
-        String line = "";
-        String kvFile = "KritiskVej.txt";
-
-        try (BufferedReader br = new BufferedReader(new FileReader(kvFile)))
-        {
-            while ((line = br.readLine()) !=null)
-            {
-                String[] kvReader = line.split(semikolon);
-
-                System.out.println("event: " + kvReader[0] + ", task: " + kvReader[1] + ", duration: " + kvReader[2]);
-
-
-            }
-
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
     }
 }
